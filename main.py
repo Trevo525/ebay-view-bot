cf38c176d0744ebc1edd746353cd1f27ea7e74db
Trevor Vance <lobovance2@yahoo.com>
2020-01-27 22:17:28 -0600

    Added support for multithread
    Add the multiprocessing.dummy module from Python to have multithreading.

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
+  GNU nano 4.7                                                                                                       main.py                                                                                                                 
 import requests
 import time
+import multiprocessing.dummy as mp
+
+#Made a global variable to pass through threading
+listingURL = "www.ebay.com"
 
 def view():
     print
     print (" --------------------------------------------")
     print (" viewbot v.1.0.0")
     print (" --------------------------------------------\n")
+    global listingURL
     listingURL = input(" Link : ")
     print
     # viewCount = int(raw_input(" How many views? : "))
@@ -17,9 +23,13 @@ def view():
 
     print
     start_time = time.time()
-    for i in range(viewCount):
-        r = requests.get(listingURL)
     
+    #adds threading support
+    p=mp.Pool(4)
+    p.map(addViews, range(0,viewCount))
+    p.close()
+    p.join()
+
     print (" Task completed! ")
     viewTime = float(time.time() - start_time)
     print(" Total time : " + " %s sec" % viewTime)
@@ -27,8 +37,12 @@ def view():
     print (" View rate  : " +  " %s views/sec" % viewRate)
     print
     print
-    
+
     # todo : iterate through other links in txt file, multithread, proxy support
+
+
+def addViews(numIterations):
+    r = requests.get(listingURL)
 
 
 if __name__ == '__main__':
